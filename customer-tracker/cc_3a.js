2348b320633cae032e22a29ee57c2fef4f9710b8
Andrew <andrewmarello@usf.edu>
2026-02-08 17:18:22 -0500

    Updated index.html and js file

--- customer-tracker/cc_3a.js
+++ customer-tracker/cc_3a.js
@@ -15,3 +15,31 @@ let customers = [
         purchases: ["Shirts", "Cleats", "Shorts"]
     }
 ];
+
+// Step 3: Add and Remove Data
+customers.push(
+    {name: "Carlos Alcaraz",
+        email: "carlosalcaraz@gmail.com",
+        purchases: ["Racquets", "Wristbands", "Polos"]
+    });
+    
+    customers.shift();
+
+// Step 4: Update Customer Info
+customers.forEach(customer => {
+    if (customer.name === "Travis Etienne") {
+        customer.email = "travetn@gmail.com";
+     }
+});
+customers.forEach(customer => {
+    if (customer.name === "Tiger Woods") {
+        customer.purchases.push("Snacks");
+    }
+});
+
+// Step 5: Display Customer Information
+customers.forEach(customer => {
+    console.log(`Name: ${customer.name}, Email: ${customer.email}, Total Purchases: ${customer.purchases.length}`
+    
+    );
+});

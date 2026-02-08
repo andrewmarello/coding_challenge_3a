21c9c1925ae575a11b96828ae1c34c684f7eb8de
Andrew <andrewmarello@usf.edu>
2026-02-08 16:29:59 -0500

    Creating Customer Records

---
+++ README.md
@@ -0,0 +1,4 @@
+# Coding Challenge 3a
+
+Coding Challenge 03a - Customer Records Tracker
+
--- customer-tracker/cc_3a.js
+++ customer-tracker/cc_3a.js
@@ -1,3 +1,17 @@
-console.log('3a Customer Records Tracker - Andrew Marello');
+console.log(`3a Customer Records Tracker - Andrew Marello`);
 
 // Step 2: Create Customer Records
+let customers = [
+    {name: "Aaron Judge",
+        email: "aaronjudge@gmail.com",
+        purchases: ["Jerseys", "Shoes", "Socks"]
+    },
+    {name: "Tiger Woods",
+        email: "tigerwoords@gmail.com",
+        purchases: ["Clubs", "Balls", "Hats"]
+    },
+    {name: "Travis Etienne",
+        email: "travissetienne@gmail.com",
+        purchases: ["Shirts", "Cleats", "Shorts"]
+    }
+];
